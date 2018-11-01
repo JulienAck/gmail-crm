d110c184fc3ed1bf46cb54020f12fdd5660649bc
Henri M <dev@webving.fr>
2018-11-01 09:45:02 +0100

    basic link to search all conversations by sender

---
+++ contentScript.js
@@ -0,0 +1,18 @@
+function findEmailBlocks() {
+	let senderBlock = document.querySelectorAll('[class="iw"]');
+	if (senderBlock[0] != undefined) {
+		let spanList = senderBlock[0].getElementsByTagName("span");
+		for (let i = 0 ; i<spanList.length; i++) {
+			let emailAddress = spanList[i].getAttribute("email");
+			if (emailAddress!=undefined) {
+				var linkToCrm = document.createElement('a');
+				linkToCrm.setAttribute('style','margin:0 0 0 10px;');
+				linkToCrm.setAttribute('href','https://mail.google.com/mail/u/0/#search/'+emailAddress);
+				linkToCrm.textContent="All conversations";
+				spanList[i].parentNode.appendChild(linkToCrm);
+			}
+		}
+	} 
+}
+console.log('GMail CRM activated');
+window.onhashchange = findEmailBlocks;
